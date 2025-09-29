5eaf4f40bb1ac4f236dc64d5f2cdef2f28e8f0bf
Johnny Tahirov <jeyhunt@gmail.com>
2025-09-29 12:26:22 -0700

    reshuffle the theme orchistration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
 import { Typography, Container } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import theme from "./theme";
+import Header from "./components/Header";
 
 function App() {
   return (
     <React.Fragment>
-      <CssBaseline enableColorScheme />
-      <ThemeProvider theme={theme}>
-        <Container>
-          <Typography variant="body1">Hello</Typography>
-          <Button variant="contained"> hello </Button>
-        </Container>
-      </ThemeProvider>
+      <Header />
+      <Container>
+        <Typography variant="body1">Hello</Typography>
+        <Button variant="contained"> hello </Button>
+      </Container>
     </React.Fragment>
   );
 }
