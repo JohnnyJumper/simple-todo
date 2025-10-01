8add9aeada319cec131956ca90ee2eef8abf71fb
Johnny Tahirov <jeyhunt@gmail.com>
2025-09-30 20:22:44 -0700

    prepare backend for docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (process.env.CORS_ORIGINS ?? '').split(',').map((s) => s.trim()),
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
